dc9feaf7d25e19f4f78fde27e8f88c80d29bc021
user <47899331+pat-bert@users.noreply.github.com>
2019-11-18 15:23:10 +0100

    Deleted language dict idea
    Signed-off-by: user <47899331+pat-bert@users.noreply.github.com>

--- src/MelfaCmd.py
+++ src/MelfaCmd.py
@@ -95,35 +95,35 @@ class InterpolationMov:
         inc_vector = vector / count_segments
         return [start + i * inc_vector for i in range(count_segments)]
 
-    @classmethod
-    def mv_crl(cls, start: Coordinate, target: Coordinate, offset: Coordinate, velocity: float, delta_t: float = 7.1e-3,
-               mv_dir=DIR_CLOCKWISE) -> List[Coordinate]:
-        """
-        Circular interpolation from start coordinates to target coordinates
-        :param start: Start coordinates
-        :param target: Target coordinates
-        :param offset: Centre offset
-        :param velocity: Speed for motion
-        :param delta_t: Time interval
-        :param mv_dir: Direction of circular movement
-        :return: List of coordinates
-        """
-        centre = start + offset
-        cs = offset
-        ct = centre - target
-
-        if cs.vector_len() != ct.vector_len():
-            raise ValueError("Centre of arc is not equidistant from initial and final position.")
-        else:
-            radius = cs
-            # Get angle between vectors
-            phi = np.rad2deg(cs.vector_angle_rad(ct))
-            # Get correct angle for direction
-            if mv_dir == cls.DIR_CLOCKWISE:
-                phi = 360 - phi
-            # Get phi increments
-            arc_len = phi * radius
-            t_total = arc_len / velocity
-            count_segments = ceil(t_total / delta_t)
-            delta_phi = phi / count_segments
-            # Generate intermediate positions
+    # @classmethod
+    # def mv_crl(cls, start: Coordinate, target: Coordinate, offset: Coordinate, velocity: float, delta_t: float = 7.1e-3,
+    #            mv_dir=DIR_CLOCKWISE) -> List[Coordinate]:
+    #     """
+    #     Circular interpolation from start coordinates to target coordinates
+    #     :param start: Start coordinates
+    #     :param target: Target coordinates
+    #     :param offset: Centre offset
+    #     :param velocity: Speed for motion
+    #     :param delta_t: Time interval
+    #     :param mv_dir: Direction of circular movement
+    #     :return: List of coordinates
+    #     """
+    #     centre = start + offset
+    #     cs = offset
+    #     ct = centre - target
+    #
+    #     if cs.vector_len() != ct.vector_len():
+    #         raise ValueError("Centre of arc is not equidistant from initial and final position.")
+    #     else:
+    #         radius = cs
+    #         # Get angle between vectors
+    #         phi = np.rad2deg(cs.vector_angle_rad(ct))
+    #         # Get correct angle for direction
+    #         if mv_dir == cls.DIR_CLOCKWISE:
+    #             phi = 360 - phi
+    #         # Get phi increments
+    #         arc_len = phi * radius
+    #         t_total = arc_len / velocity
+    #         count_segments = ceil(t_total / delta_t)
+    #         delta_phi = phi / count_segments
+    #         # Generate intermediate positions
