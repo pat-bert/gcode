fde91f947d7c69cf57410023863dc7bdcdd7e965
Patrick Bertsch <47899331+pat-bert@users.noreply.github.com>
2020-02-04 12:27:42 +0100

    Test for too long message Cleaned up requirements (pip freeze and check)
    Signed-off-by: Patrick Bertsch <47899331+pat-bert@users.noreply.github.com>

--- src/AM_IR/mutmut_exec.bat
+++ src/AM_IR/mutmut_exec.bat
@@ -1,1 +1,1 @@
-.\..\..\venv\Scripts\python.exe -m mutmut run --paths-to-mutate .\..\AM_IR\gcode\GCmd.py
+.\..\..\venv\Scripts\python.exe -m mutmut run --paths-to-mutate .\..\AM_IR\melfa\TcpClientR3.py
--- src/AM_IR/test/test_TcpClientR3.py
+++ src/AM_IR/test/test_TcpClientR3.py
@@ -1,21 +1,33 @@
-from unittest import TestCase
+import pytest
+
+from AM_IR.melfa.TcpClientR3 import TcpClientR3
 
 
-class TestTcpClientR3(TestCase):
+class TestTcpClientR3:
     def test_connect(self):
-        self.fail()
+        assert False
 
     def test_close(self):
-        self.fail()
+        assert False
 
     def test_send(self):
-        self.fail()
+        assert False
+
+    def test_send_before_connect(self):
+        assert False
+
+    def test_send_message_too_long(self):
+        tcp = TcpClientR3()
+        tcp.connect()
+
+        with pytest.raises(ValueError):
+            tcp.send("T" * 128)
 
     def test_wait_send(self):
-        self.fail()
+        assert False
 
     def test_receive(self):
-        self.fail()
+        assert False
 
     def test_mainloop(self):
-        self.fail()
+        assert False
