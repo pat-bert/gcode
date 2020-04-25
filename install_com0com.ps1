99e36619493385f167e931a3fff13886ef81d965
Patrick Bertsch <47899331+pat-bert@users.noreply.github.com>
2020-04-25 11:13:11 +0200

    gcode-79 Integration test robot functions - Some commands that are working for com0com (still needs to check paths, rights, driver signing)
    Signed-off-by: Patrick Bertsch <47899331+pat-bert@users.noreply.github.com>

---
+++ install_com0com.ps1
@@ -0,0 +1,5 @@
+﻿# Set the environment variable to install COM#<->COM#-like serial ports
+$env:CNC_INSTALL_COMX_COMX_PORTS='YES'
+
+# Install the software silently (requires administrator)
+.\Setup_com0com_v3.0.0.0_W7_x64_signed.exe /S
