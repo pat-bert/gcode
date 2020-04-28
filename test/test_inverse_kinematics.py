7af2d1fbc837ecd0ab80818d418fe7622d2f34e5
Patrick Bertsch <47899331+pat-bert@users.noreply.github.com>
2020-04-28 22:22:23 +0200

    Added IK stress test (16k configurations), still lots of failures Calculating 2/3 pose flags based on joint angles
    Signed-off-by: Patrick Bertsch <47899331+pat-bert@users.noreply.github.com>

--- test/test_inverse_kinematics.py
+++ test/test_inverse_kinematics.py
@@ -1,4 +1,4 @@
-from math import pi, cos, sin
+from math import pi, cos, sin, atan
 
 import numpy as np
 import pytest
@@ -12,6 +12,21 @@ def get_tform(xdir, ydir, zdir, pos):
     tform[3, 3] = 1
     tform[0:3] = np.array([xdir, ydir, zdir, pos]).transpose()
     return tform
+
+
+def calculate_pose_flags(config, joint_values) -> float:
+    # Convert to DH-system
+    joint_values = [joint_val + joint.zero_offset for joint_val, joint in zip(joint_values, config)]
+
+    right = 1
+
+    # Theta 3 determines elbow position (above or below plane of joint 2 and joint 3)
+    above = 1 if joint_values[2] > - atan(config[3].d / config[2].a) else 0
+
+    # Theta 5 determines flip/nonflip
+    non_flip = 1 if joint_values[4] > 0 else 0
+
+    return non_flip + above * 2 + right * 4
 
 
 def idfn(val):
@@ -128,3 +143,46 @@ def test_ik_spherical_wrist_bad_config(dummy_tform, dh_melfa_rv_4a):
 
     with pytest.raises(ValueError):
         ik_spherical_wrist(dh_melfa_rv_4a + [dh_melfa_rv_4a[-1]], dummy_tform, pose_flags=7)
+
+
+@pytest.mark.parametrize("theta6", [-200, -100, 0, 100, 200])
+@pytest.mark.parametrize("theta5", [-120, -90, 0, 90, 120])
+@pytest.mark.parametrize("theta4", [-160, -90, 0, 90, 160])
+@pytest.mark.parametrize("theta3", [15, 45, 90, 130, 169])
+@pytest.mark.parametrize("theta2", [-90, -45, 0, 60, 140])
+@pytest.mark.parametrize("theta1", [-160, -90, 0, 90, 160])
+def test_ik_spherical_wrist_stress(theta1, theta2, theta3, theta4, theta5, theta6, dh_melfa_rv_4a):
+    # Collect angles
+    expected_joints = [theta1, theta2, theta3, theta4, theta5, theta6]
+    expected_joints = np.deg2rad(expected_joints)
+
+    # Calculate roboter pose
+    tform_under_test = forward_kinematics(dh_melfa_rv_4a, expected_joints)
+
+    # Calculate pose flags
+    flags = calculate_pose_flags(dh_melfa_rv_4a, expected_joints)
+
+    # Calculate inverse kinematics
+    actual_joints = ik_spherical_wrist(dh_melfa_rv_4a, tform_under_test, pose_flags=flags)
+
+    # print(f'Actual:  {[f"{actual:+.3f}" for actual in actual_joints]}')
+    # print(f'Expect:  {[f"{expect:+.3f}" for expect in expected_joints]}')
+    np.testing.assert_allclose(actual_joints, expected_joints, atol=0.01)
+
+
+@pytest.mark.parametrize("joints,ex_flags",
+                         [
+                             [(-160, 0, 28, 0, 20, 0), 5],
+                             [(-160, 0, 29, 0, 20, 0), 7],
+                             [(-160, 0, 29, 0, -20, 0), 6],
+                             [(-160, 0, 28, 0, -20, 0), 4],
+                         ]
+                         )
+def test_calculate_pose_flags(joints, ex_flags, dh_melfa_rv_4a):
+    # Collect angles
+    joints = np.deg2rad(joints)
+
+    # Calculate flags
+    flags = calculate_pose_flags(dh_melfa_rv_4a, joints)
+
+    assert ex_flags == flags
