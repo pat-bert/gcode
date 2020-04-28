9a5eb24bb89f7bd5ae223617bd88b6b24e0f445b
Patrick Bertsch <47899331+pat-bert@users.noreply.github.com>
2020-04-28 16:10:47 +0200

    - FK: Option to subtract offset (angles are already in DH-form) - IK: Using subtract option to use DH-angles consistently - IK: Fixing matrix was calculated element-wise instead of using np.dot - IK: Adding calculation for theta 4
    Signed-off-by: Patrick Bertsch <47899331+pat-bert@users.noreply.github.com>

--- src/kinematics/forward_kinematics.py
+++ src/kinematics/forward_kinematics.py
@@ -8,12 +8,13 @@ from numpy.linalg import multi_dot
 from src.kinematics.joints import BaseJoint
 
 
-def forward_kinematics(config: List[BaseJoint], joint_coordinates: List[float]) -> ndarray:
+def forward_kinematics(config: List[BaseJoint], joint_coordinates: List[float], subtract_offset=False) -> ndarray:
     """
     Calculate the forward kinematics for a given configuration of joints and joint coordinates.
     :param config: Tuple of joints
     :param joint_coordinates: Tuple of joint coordinate values (either mm or radian)
     :return: 4x4 Transformation matrix (vectors for TCP coordinate system and vector for TCP position)
+    :param subtract_offset:
     :raises: ValueError if the lengths of config and joint_coordinates are unequal
 
     T = | xx yx zx x |
@@ -24,7 +25,10 @@ def forward_kinematics(config: List[BaseJoint], joint_coordinates: List[float]) 
     if len(config) == len(joint_coordinates):
         for joint, coordinate_value in zip(config, joint_coordinates):
             # Pass in the joint coordinates to get the complete matrices
-            joint.mul(joint_value=coordinate_value)
+            if subtract_offset:
+                joint.mul(joint_value=coordinate_value - joint.zero_offset)
+            else:
+                joint.mul(joint_value=coordinate_value)
         if len(config) > 1:
             # Do an optimized calculation of the product of all matrices
             return multi_dot([joint.matrix for joint in config])
--- src/kinematics/inverse_kinematics.py
+++ src/kinematics/inverse_kinematics.py
@@ -65,7 +65,7 @@ def ik_spherical_wrist(config: List[BaseJoint], tform: np.ndarray, pose_flags=No
     theta1 = atan2(p04[1], p04[0])
 
     # Get origin of joint 2 in base frame
-    tjoint12 = forward_kinematics([config[0]], [theta1])
+    tjoint12 = forward_kinematics([config[0]], [theta1], subtract_offset=True)
     p01 = tjoint12[0:3, 3]
 
     # Get the vector from origin of joint 2 (frame 1) to wrist center
@@ -77,54 +77,19 @@ def ik_spherical_wrist(config: List[BaseJoint], tform: np.ndarray, pose_flags=No
     # Calculate theta 2
     theta2, *_ = _ik_spherical_wrist_joint2(config, joint2_first, tjoint12, p14)
 
-    # TODO Calculate theta 4
-    theta4 = 0
-
     # Calculate theta 5 (requires theta 1 - 3)
     theta5, *_ = _ik_spherical_wrist_joint5(config, joint5_non_flip, tjoint12, theta2, theta3, zdir)
 
-    # Calculate theta 6 (requires theta 1 - 5)
-    # TODO Solve the wrist angles for the TCP orientation (joint 6)
+    # Calculate theta 4 (requires theta 1 - 3)
+    theta4, *_ = _ik_spherical_wrist_joint4(config, tjoint12, theta2, theta3, zdir)
+
+    # TODO Calculate theta 6 (requires theta 1 - 5)
     theta6, *_ = _ik_spherical_wrist_joint6(config, tjoint12, theta2, theta3, theta4, theta5, xdir)
 
     # Bundle all the angles and apply the offset
     theta = [theta1, theta2, theta3, theta4, theta5, theta6]
     print(f'Offsets: {[f"{joint.zero_offset:+.3f}" for joint in config]}')
     return [angle - joint.zero_offset for angle, joint in zip(theta, config)]
-
-
-def _ik_spherical_wrist_joint3(config, joint3_up, p14) -> List[float]:
-    """
-    Calculate the third joint for the spherical wrist robot type
-    :param config: Robot configuration to access DH-parameters
-    :param joint3_up: Pose flog
-    :param p14: Vector from frame origin in joint 2 to wrist center
-    :return: List of solutions.
-    If a configuration is given by non_flip only one solutions is returned.
-    Otherwise both solutions are returned.
-    """
-    # Calculate other sides of triangle from link lengths
-    l4 = sqrt(config[3].d ** 2 + config[2].a ** 2)
-    l2 = config[1].a
-
-    # Get both solutions for the auxiliary angle phi using cosine law
-    vector_len_p14 = np.linalg.norm(p14)
-    aux_phi_1 = acos((l2 ** 2 + l4 ** 2 - vector_len_p14 ** 2) / (2 * l2 * l4))
-    aux_phi_2 = 2 * pi - aux_phi_1
-
-    # Calculate solutions for theta3 based on phi
-    phi_offset = (pi - atan(config[3].d / config[2].a))
-    theta3_1 = phi_offset - aux_phi_1
-    theta3_2 = phi_offset - aux_phi_2
-
-    # TODO Select the solution based on the pose flag
-    if joint3_up is not None:
-        if joint3_up:
-            print(f'Theta 3: [x] {theta3_1:+.3f} [ ] {theta3_2:+.3f}')
-            return [theta3_1]
-        print(f'Theta 3: [ ] {theta3_1:+.3f} [x] {theta3_2:+.3f}')
-        return [theta3_2]
-    return [theta3_1, theta3_2]
 
 
 def _ik_spherical_wrist_joint2(config, joint2_first, tjoint12, p14) -> List[float]:
@@ -166,6 +131,71 @@ def _ik_spherical_wrist_joint2(config, joint2_first, tjoint12, p14) -> List[floa
     return [theta2_1, theta2_2]
 
 
+def _ik_spherical_wrist_joint3(config, joint3_up, p14) -> List[float]:
+    """
+    Calculate the third joint for the spherical wrist robot type
+    :param config: Robot configuration to access DH-parameters
+    :param joint3_up: Pose flog
+    :param p14: Vector from frame origin in joint 2 to wrist center
+    :return: List of solutions.
+    If a configuration is given by non_flip only one solutions is returned.
+    Otherwise both solutions are returned.
+    """
+    # Calculate other sides of triangle from link lengths
+    l4 = sqrt(config[3].d ** 2 + config[2].a ** 2)
+    l2 = config[1].a
+
+    # Get both solutions for the auxiliary angle phi using cosine law
+    vector_len_p14 = np.linalg.norm(p14)
+    aux_phi_1 = acos((l2 ** 2 + l4 ** 2 - vector_len_p14 ** 2) / (2 * l2 * l4))
+    aux_phi_2 = 2 * pi - aux_phi_1
+
+    # Calculate solutions for theta3 based on phi
+    phi_offset = (pi - atan(config[3].d / config[2].a))
+    theta3_1 = phi_offset - aux_phi_1
+    theta3_2 = phi_offset - aux_phi_2
+
+    # TODO Select the solution based on the pose flag
+    if joint3_up is not None:
+        if joint3_up:
+            print(f'Theta 3: [x] {theta3_1:+.3f} [ ] {theta3_2:+.3f}')
+            return [theta3_1]
+        print(f'Theta 3: [ ] {theta3_1:+.3f} [x] {theta3_2:+.3f}')
+        return [theta3_2]
+    return [theta3_1, theta3_2]
+
+
+def _ik_spherical_wrist_joint4(config, tjoint12, theta2, theta3, zdir) -> List[float]:
+    """
+    Calculate the fourth joint for the spherical wrist robot type
+    :param config: Robot configuration to access DH-parameters
+    :param tjoint12: Previously calculated transformation
+    :param theta2: Previously calculated shoulder angle
+    :param theta3: Previously calculated elbow angle
+    :param zdir: Unit vector for z-axis of flange frame given in base frame
+    :return: List of solutions.
+    If a configuration is given by non_flip only one solutions is returned.
+    Otherwise both solutions are returned.
+    """
+    # Get the vector from joint 1 to joint 4 (same plane) based on previous results (neutralize offset)
+    tjoint24 = forward_kinematics(config[1:3], [theta2, theta3], subtract_offset=True)
+    tjoint14 = np.dot(tjoint12, tjoint24)
+
+    # Get z-direction of joint 4 in base frame
+    z3 = tjoint14[0:3, 2]
+    y3 = tjoint14[0:3, 1]
+
+    c = np.cross(zdir, z3)
+
+    if np.linalg.norm(c) != 0:
+        q4 = acos(np.dot(y3, c))
+    else:
+        # Singularity (J4 and J6 are collinear)
+        q4 = 0
+
+    return [q4]
+
+
 def _ik_spherical_wrist_joint5(config, non_flip, tjoint12, theta2, theta3, zdir) -> List[float]:
     """
     Calculate the fifth joint for the spherical wrist robot type
@@ -180,8 +210,8 @@ def _ik_spherical_wrist_joint5(config, non_flip, tjoint12, theta2, theta3, zdir)
     Otherwise both solutions are returned.
     """
     # Get the vector from joint 1 to joint 4 (same plane) based on previous results
-    tjoint24 = forward_kinematics(config[1:3], [theta2, theta3])
-    tjoint14 = tjoint12 * tjoint24
+    tjoint24 = forward_kinematics(config[1:3], [theta2, theta3], subtract_offset=True)
+    tjoint14 = np.dot(tjoint12, tjoint24)
 
     # Get z-direction of joint 4 in base frame
     z3 = tjoint14[0:3, 2]
@@ -215,8 +245,8 @@ def _ik_spherical_wrist_joint6(config, tjoint12, theta2, theta3, theta4, theta5,
     Otherwise both solutions are returned.
     """
     # Get frame of joint 5
-    tjoint26 = forward_kinematics(config[1:5], [theta2, theta3, theta4, theta5])
-    tjoint16 = tjoint12 * tjoint26
+    tjoint26 = forward_kinematics(config[1:5], [theta2, theta3, theta4, theta5], subtract_offset=True)
+    tjoint16 = np.dot(tjoint12, tjoint26)
     x5 = tjoint16[0:3, 1]
 
     # Theta 6 describes angle between x-dir of joint 5 and joint 6
