794183d39389c4e0c44a2756b6e03ade2e6650f8
Patrick Bertsch <47899331+pat-bert@users.noreply.github.com>
2020-06-18 10:47:28 +0200

    Calculating effort and tool vector internally now. Accepting weights.
    Signed-off-by: Patrick Bertsch <47899331+pat-bert@users.noreply.github.com>

--- src/prechecks/frr.py
+++ src/prechecks/frr.py
@@ -9,47 +9,24 @@ from src.kinematics.joints import BaseJoint
 from src.prechecks.configs import melfa_rv_4a
 
 
-def frr(config: List[BaseJoint], joints: List[float], tool_vector: List[float], h: List[float]) -> List[float]:
+def frr(config: List[BaseJoint], initial_joints: List[float], weights=None, stop_threshold: float = 1e-6) \
+        -> List[float]:
     """
     Functional Redundancy Resolution
     :param config: Tuple of joints, offsets are considered
-    :param joints: Tuple of joint coordinate values (either mm or radian)
-    :param tool_vector:
-    :param h:
+    :param initial_joints: Tuple of joint coordinate values (either mm or radian)
+    :param weights:
+    :param stop_threshold:
     :return:
     """
-    # Calculate jacobian and its righ-hand pseudo-inverse
-    jac = geometric_jacobian(config, joints)
-    pinv_jac = right_generalized_inverse_jacobian(jac)
-
-    # Calculate tool vector, its transposed version and the projector
-    total_tform = forward_kinematics(config, joints)
-    e = np.asarray(tool_vector)
+    # Obtain tool column vector as z-axis of endeffector frame
+    total_tform = forward_kinematics(config, initial_joints)
     e = total_tform[0:3, 2]
     e = e[:, None]
 
-    if e.shape != (3, 1):
-        raise TypeError(f'Tool vector must be (3,1) not {e.shape}.')
-
+    # Calculate projector onto tool axis
     t_proj = e @ e.T
 
-    # Transform the manipulation vector and calculate the delta joints
-    h = np.asarray(h)
-    h = h[:, None]
-
-    if h.shape != (len(joints), 1):
-        raise TypeError(f'h must be a {len(joints)}x1 vector.')
-
-    delta_joints = pinv_jac @ np.vstack([np.zeros((3, 1)), t_proj @ jac[3:, :] @ (0.1 * np.eye(6) @ h)])
-
-    # Return flat list
-    return delta_joints.T[0, :].tolist()
-
-
-if __name__ == '__main__':
-    robot = melfa_rv_4a()
-    home = [0, 0, pi / 2, 0, pi / 2, 0]
-    tool = [0, 0, 1]
     joint_limits = [
         -2.7925, 2.7925,
         -1.5708, 2.4435,
@@ -59,11 +36,54 @@ if __name__ == '__main__':
         -3.4907, 3.4907
     ]
     median = [(lower + upper) / 2 for lower, upper in zip(joint_limits[::2], joint_limits[1::2])]
+
+    # Initialize current joint column vector
+    current_joints = np.asarray(initial_joints)
+    current_joints = current_joints[:, None]
+
+    # Optimize the joint vector in the orthogonal space
     while True:
-        manip = [qm - q for qm, q in zip(median, home)]
-        print(f'Manip: {manip}')
-        delta = frr(robot, home, tool, manip)
-        print(f'Delta (deg): {[round(dq / pi * 180, ndigits=3) for dq in delta]}')
-        home = [q + dq for q, dq in zip(home, delta)]
-        print(f'Joints (deg): {[round(q / pi * 180, ndigits=3) for q in home]}')
-        input('Continue?')
+        # Calculate jacobian and its righ-hand pseudo-inverse for current joint values
+        jac = geometric_jacobian(config, current_joints[:, 0].tolist())
+        pinv_jac = right_generalized_inverse_jacobian(jac)
+
+        # Create the manipulation column vector
+        manip = [qm - q for qm, q in zip(median, current_joints[:, 0].tolist())]
+        h = np.asarray(manip)
+        h = h[:, None]
+
+        if h.shape != (len(initial_joints), 1):
+            raise TypeError(f'h must be a {len(initial_joints)}x1 vector.')
+
+        # Set weights as diagonal matrix
+        if weights is None:
+            weights = [0.1] * len(initial_joints)
+        elif any((i >= 1 for i in weights)):
+            raise ValueError('Weights need to be < 1 for the algorithm to converge.')
+        w = np.diag(weights)
+
+        # Calculate delta for iteration
+        effect = np.linalg.multi_dot([t_proj, jac[3:, :], w, h])
+        delta_joints = pinv_jac @ np.vstack([np.zeros((3, 1)), effect])
+        current_joints += delta_joints
+
+        if np.linalg.norm(delta_joints) < stop_threshold:
+            # Stop when the correcting effort is below a certain threshold
+            break
+
+    # Return flat list
+    return current_joints[:, 0].tolist()
+
+
+if __name__ == '__main__':
+    robot = melfa_rv_4a()
+    home = [-1, pi / 3, pi / 2, 0, pi / 2, pi]
+    # home = [-64, 47, 79, 31, 55, -180]
+    print(home)
+    # home = [i / 180 * pi for i in home]
+    import time
+
+    s = time.time()
+    new_joints = frr(robot, home, weights=[0.1, 0.1, 0.1, 0.1, 0.1, 0.5])
+    print(f'Calculated in {time.time() - s} s.')
+    print(new_joints)
