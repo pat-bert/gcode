45fba0c61543e929177573037b59379df83b4ba1
Patrick Bertsch <47899331+pat-bert@users.noreply.github.com>
2020-07-05 10:54:15 +0200

    Changed file path
    Signed-off-by: Patrick Bertsch <47899331+pat-bert@users.noreply.github.com>

--- test/test_check_trajectory.py
+++ test/test_check_trajectory.py
@@ -6,7 +6,7 @@ from src.cli_commands.check_trajectory import check_trajectory
 
 def test_check_trajectory_out_of_reach():
     with pytest.raises(WorkspaceViolation) as e:
-        check_trajectory(config_f='config.ini', gcode_f='joint_violation.gcode')
+        check_trajectory(config_f='config.ini', gcode_f='./test/joint_violation.gcode')
 
     assert 'segment #0' in str(e.value)
     assert 'Pos 471' in str(e.value)
