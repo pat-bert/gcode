Adding singularities to pose calculation Adding better check for invalid cross product Fixing collision index was off by one (robot arms are counted separately from base)
Signed-off-by: Patrick Bertsch <47899331+pat-bert@users.noreply.github.com>

src/kinematics/forward_kinematics.py
@@ -6,6 +6,7 @@ from numpy.core.multiarray import ndarray
 from numpy.linalg import multi_dot, inv
 
 from src.kinematics.joints import BaseJoint, JointType
+from src.kinematics.joints import WristSingularity, ElbowSingularity, ShoulderSingularity
 
 
 def geometric_jacobian(config: List[BaseJoint], joint_coordinates: List[float]) -> ndarray:
@@ -212,7 +213,7 @@ def get_tform(xdir: List[float], ydir: List[float], zdir: List[float], pos: List
     return tform
 
 
-def calculate_pose_flags(config, joint_values) -> float:
+def calculate_pose_flags(config: List[BaseJoint], joint_values: List[float]) -> float:
     """
     Calculate the flags of a roboter pose
     :param config: Tuple of joints
@@ -230,18 +231,32 @@ def calculate_pose_flags(config, joint_values) -> float:
     z_axis_j2 = tjoint12[0:3, 2]
     normal_vec = np.cross(z_axis_j2, z_axis_j1)
 
-    # TODO Consider singularities
     # Use dot product to determine side
-    right = 1 if np.dot(normal_vec, wrist_center_pos) > 0 else 0
+    if np.dot(normal_vec, wrist_center_pos) > 0:
+        right = 1
+    elif np.dot(normal_vec, wrist_center_pos) < 0:
+        right = 0
+    else:
+        raise ShoulderSingularity
 
     # Convert to DH-system
     joint_values_dh = [joint_val + joint.zero_offset for joint_val, joint in zip(joint_values, config)]
 
     # Theta 3 determines elbow position (above or below plane of joint 2 and joint 3)
-    above = 1 if joint_values_dh[2] > - atan(config[3].d / config[2].a) else 0
+    if joint_values_dh[2] > - atan(config[3].d / config[2].a):
+        above = 1
+    elif joint_values_dh[2] < - atan(config[3].d / config[2].a):
+        above = 0
+    else:
+        raise ElbowSingularity
 
     # Theta 5 determines flip/nonflip
-    non_flip = 1 if joint_values_dh[4] > 0 else 0
+    if joint_values_dh[4] > 0:
+        non_flip = 1
+    elif joint_values_dh[4] < 0:
+        non_flip = 0
+    else:
+        raise WristSingularity
 
     return non_flip + above * 2 + right * 4
 

src/kinematics/inverse_kinematics.py
@@ -3,32 +3,12 @@ from typing import List, Optional, Dict
 
 import numpy as np
 
+from src.kinematics.joints import ShoulderSingularity, WristSingularity, WRIST_SINGULARITY_THRESHOLD, \
+    SHOULDER_SINGULARITY_THRESHOLD
 from src.kinematics.forward_kinematics import forward_kinematics, vec3_cross
 from src.kinematics.joints import BaseJoint
 
 JointSolution = Dict[int, List[float]]
-
-
-class Singularity(ValueError):
-    pass
-
-
-class ShoulderSingularity(Singularity):
-    """
-    Will be raised if wrist center point is on J1 axis
-    """
-
-
-class WristSingularity(Singularity):
-    """
-    Will be raised if J4 and J6 align (infinite solutions)
-    """
-
-
-class ElbowSingularity(Singularity):
-    """
-    Will be raised if the wrist center point is within the plane through J2 and J3
-    """
 
 
 class OutOfReachError(ValueError):
@@ -40,11 +20,6 @@ class OutOfReachError(ValueError):
 # Apply the offset and wrap to (-pi, pi]
 def wrap_to_pi(angle):
     return -((- angle + pi) % (2 * pi) - pi)
-
-
-WRIST_SINGULARITY_THRESHOLD = 1e-3
-ELBOW_SINGULARITY_THRESHOLD = 1e-3
-SHOULDER_SINGULARITY_THRESHOLD = 1e-3
 
 
 def ik_spherical_wrist(config: List[BaseJoint], tform: np.ndarray, pose_flags=None) -> JointSolution:

src/prechecks/world_collision.py
@@ -22,7 +22,10 @@ def create_cuboid_from_path(line: LinearSegment, nvec: np.ndarray, extr: Extrusi
     # Get the lateral direction
     lateral_vec = np.cross(nvec, upper_mid_end - upper_mid_start)
     try:
-        ydir = extr.width * lateral_vec / np.linalg.norm(lateral_vec)
+        vector_len = np.linalg.norm(lateral_vec)
+        if vector_len == 0:
+            raise ValueError('Invalid normal vector. Cross product had zero length.')
+        ydir = extr.width * lateral_vec / vector_len
     except ZeroDivisionError:
         raise ValueError('Invalid normal vector. Cross product had zero length.')
 